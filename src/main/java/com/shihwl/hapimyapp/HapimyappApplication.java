77c6b949270c6905b42b2b65614c796c8fea0801
史文亮 <shiwenliang@shiwenliangdeMacBook-Pro.local>
2020-07-16 13:42:45 +0800

    Initial commit

---
+++ src/main/java/com/shihwl/hapimyapp/HapimyappApplication.java
@@ -0,0 +1,13 @@
+package com.shihwl.hapimyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapimyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HapimyappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shihwl/hapimyapp/HapimyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shihwl.hapimyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HapimyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
